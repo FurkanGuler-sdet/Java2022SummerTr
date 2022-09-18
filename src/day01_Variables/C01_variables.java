35a92db76a336632ced309a8f713db2797a6e859
Furkan Guler <113851188+FurkanGuler-sdet@users.noreply.github.com>
2022-09-18 20:23:11 +0300

    Update C01_variables.java

--- src/day01_Variables/C01_variables.java
+++ src/day01_Variables/C01_variables.java
@@ -4,7 +4,7 @@ public class C01_variables {
     public static void main(String[] args) {
 
         System.out.println("'Hello World !' yazdiran javayi halleder");
-        System.out.println("değişiklik");
+       
     }
 
 }
