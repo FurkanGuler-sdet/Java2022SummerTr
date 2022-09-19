355190e12b74c8fb7792e34f48912dd280134ba2
Furkan Guler <furkanguler.sdet@gmail.com>
2022-09-19 18:38:31 +0300

    19.09.2022

--- src/day01_Variables/C01_variables.java
+++ src/day01_Variables/C01_variables.java
@@ -4,7 +4,8 @@ public class C01_variables {
     public static void main(String[] args) {
 
         System.out.println("'Hello World !' yazdiran javayi halleder");
-       
+        System.out.println("'Hello World !' yazdiran javayi halleder");
+
     }
 
 }
