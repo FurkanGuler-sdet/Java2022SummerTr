0ee034a4f3a6699a49231584415471d4f7edd9dc
Furkan Guler <furkanguler.sdet@gmail.com>
2022-09-25 21:11:58 +0300

    25.09.2022

--- src/day04_DataCasting/C04_ExplicitNarrowing.java
+++ src/day04_DataCasting/C04_ExplicitNarrowing.java
@@ -1,5 +1,7 @@
 package day04_DataCasting;
 
+import java.util.Scanner;
+
 public class C04_ExplicitNarrowing {
     public static void main(String[] args) {
         int sayi1=500;
@@ -15,5 +17,22 @@ public class C04_ExplicitNarrowing {
          bunun icin degerin onune parantez icerisinde istedigimiz data turunu
          yazmamiz yeterlidir.
          */
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bölünecek tam sayıyı girin:");
+        int a = scan.nextInt();
+
+        System.out.println("Bölecek tam sayıyı girin");
+        int b = scan.nextInt();
+
+        System.out.println("a/b nin değeri= "+a/b);
+
+        double dogruSonuc= (double) a/b;
+        System.out.println("atama yapıldıktan sonra doğru sonuç="+dogruSonuc);
+
+
+
+
     }
 }
